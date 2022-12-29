7cd2a8e9457c533fe327e18e4fcea831470607f6
Antthony <antthony.canuto@gmail.com>
2022-12-29 00:03:51 -0300

    update clipboard

---
+++ script.js
@@ -0,0 +1,16 @@
+const btn = document.querySelector('.btn');
+const text = document.querySelector('.text')
+
+btn.addEventListener('click', copyText);
+
+function copyText(){
+  navigator.clipboard.writeText(text.innerHTML)
+  btn.classList.add('active')
+  btn.innerText = "Texto copiado!";
+  }
+
+  setTimeout(() =>{
+    btn.classList.remove('active')
+    btn.innerText = "Copiar texto";
+  }, 400);
+
